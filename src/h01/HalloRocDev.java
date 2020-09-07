9f859e1159fd346dc02bfb310d1d9286f13a814e
Caspar van Vuure <2140608@talnet.nl>
2020-09-07 20:52:05 +0200

    Roc-dev 1e jaar

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,43 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("Hallo wereld, wereld,\n" +
+                "De wereld is van mij.\n" +
+                "Er is ruimte zat,\n" +
+                "Dus kom er lekker bij.\n" +
+                "Hallo wereld, wereld,\n" +
+                "De wereld is voor jou\n" +
+                "Er zijn wel duizend kleuren\n" +
+                "Veel meer dan rood, wit, blauw\n" +
+                "En wat ik doe, doe, doe\n" +
+                "Doe ik samen met jou\n" +
+                "En waar ik ga, ga, ga,\n" +
+                "Ga ik samen met jou\n" +
+                "Hallo wereld\n" +
+                "Hééééy\n" +
+                "Ja jij,\n" +
+                "Je bent van ons allemaal.\n" +
+                "Hallo raam,\n" +
+                "Goeiemorgen nieuwe dag\n" +
+                "Hallo bed\n" +
+                "Waarin ik net nog lekker lag\n" +
+                "Hallo boek\n" +
+                "Met je verhalen zo mooi\n" +
+                "Verre landen, nieuwe mensen\n" +
+                "Komen wonen in mijn hoofd\n" +
+                "Als je leest\n" +
+                "Snap je de mensen om je heen\n" +
+                "Als je leest dan weet je\n" +
+                "Ik ben niet alleen\n" +
+                "Want een boek,\n" +
+                "Meteen al bij de eerste zin\n" +
+                "Dat opent alle alle grenzen\n" +
+                "En laat jou de wereld zien.\n" +
+                "Hallo wereld, wereld,\n" +
+                "De wereld is van mij.\n" +
+                "Er is ruimte zat,\n" +
+                "Dus kom er lekker bij.\n" +
+                "Hallo wereld, wereld,\n" +
+                "De…");
     }
 }
